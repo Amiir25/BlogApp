d7c6ad3ccb730ad82c98ae319655557b133402f6
Amiir25 <amirsadik636@gmail.com>
2025-07-12 18:43:19 +0300

    Add Login.jsx

---
+++ client/src/pages/Login.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import '../style.scss';
+
+const Login = () => {
+  return (
+    <>
+    <div className='auth'>
+        <h1>Login</h1>
+        <form action="">
+            <input type="text" placeholder='username' />
+            <input type="password" placeholder='password' />
+            <button>Login</button>
+        </form>
+    </div>
+    </>
+  )
+}
+
+export default Login
